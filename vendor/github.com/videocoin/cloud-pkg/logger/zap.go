4692ec1392bd3fc749d57ec27e3fc4b5aee65e4f
Nikita Savik <nikita@liveplanet.net>
2020-04-23 23:52:27 +0300

    Update deps

--- vendor/github.com/videocoin/cloud-pkg/logger/zap.go
+++ vendor/github.com/videocoin/cloud-pkg/logger/zap.go
@@ -1,11 +1,13 @@
 package logger
 
 import (
+	"fmt"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/TheZeroSlave/zapsentry"
+	"github.com/getsentry/sentry-go"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -13,13 +15,10 @@ import (
 func NewZapLogger(serviceName string, serviceVersion string) *zap.Logger {
 	zapLogger, _ := zap.NewProduction()
 
-	sentryDSN = os.Getenv("SENTRY_DSN")
-
 	loglevel = strings.ToLower(os.Getenv("LOGLEVEL"))
 	if loglevel == "" {
 		loglevel = "info"
 	}
-
 	if loglevel == "debug" {
 		zapLogger, _ = zap.NewDevelopment()
 	}
@@ -29,13 +28,27 @@ func NewZapLogger(serviceName string, serviceVersion string) *zap.Logger {
 		zap.String("version", serviceVersion),
 	)
 
+	sentryDSN = os.Getenv("SENTRY_DSN")
 	if sentryDSN != "" {
-		sentryCfg := zapsentry.Configuration{
+		sentryZapCfg := zapsentry.Configuration{
 			Level:        zapcore.ErrorLevel,
 			FlushTimeout: 5 * time.Second,
+			Tags: map[string]string{
+				"service": serviceName,
+				"version": serviceVersion,
+			},
+		}
+		sentryCli, err := sentry.NewClient(sentry.ClientOptions{
+			Dsn:              sentryDSN,
+			AttachStacktrace: true,
+			Release:          fmt.Sprintf("%s@%s", serviceName, serviceVersion),
+		})
+		if err != nil {
+			zapLogger.Fatal("failed to new sentry client", zap.Error(err))
 		}
 
-		sentryCore, err := zapsentry.NewCore(sentryCfg, zapsentry.NewSentryClientFromDSN(sentryDSN))
+		sentryFactory := zapsentry.NewSentryClientFromClient(sentryCli)
+		sentryCore, err := zapsentry.NewCore(sentryZapCfg, sentryFactory)
 		if err != nil {
 			zapLogger.Fatal("failed to init zap", zap.Error(err))
 		}
